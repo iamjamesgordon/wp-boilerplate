0ecce533b1dad7024359b6c36b68649ecb1e18af
iamjamesgordon <james@neontribe.co.uk>
2024-02-03 16:59:17 +0000

    The roots/acorn bundle function has been removed
    The next step to removing bud and tailwind involves removing the enqueue script that is calling the bndle function used by defult to compile assets. The bundle function has been removed from app/setup.php.

--- wp-content/themes/starter-theme/app/setup.php
+++ wp-content/themes/starter-theme/app/setup.php
@@ -6,26 +6,6 @@
 
 namespace App;
 
-use function Roots\bundle;
-
-/**
- * Register the theme assets.
- *
- * @return void
- */
-add_action('wp_enqueue_scripts', function () {
-    bundle('app')->enqueue();
-}, 100);
-
-/**
- * Register the theme assets with the block editor.
- *
- * @return void
- */
-add_action('enqueue_block_editor_assets', function () {
-    bundle('editor')->enqueue();
-}, 100);
-
 /**
  * Register the initial theme setup.
  *
