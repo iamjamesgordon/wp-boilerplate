23cd55ed136ee36f449e336fe8d6409a8ae50781
iamjamesgordon <james@neontribe.co.uk>
2024-02-09 12:08:35 +0000

    Updating docker to run WordPress image appended with commands to add wp-cli to the container

--- scripts/wpcli.sh
+++ scripts/wpcli.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 source .env
 
-echo "Starting........."
+echo "Starting to access container........."
 
 # Setting a variable with the idea of the WordPress container for this project
 wordpressContainer=$(docker container ls --all --quiet --filter "name=${DOCKER_NETWORK_NAME}_${DOCKER_WORDPRESS_CONTAINER_NAME}")
 
 # Check if the WordPress container exists
 if [ $wordpressContainer ]; then
-
-    docker exec $wordpressContainer /bin/bash -c "
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
-    chmod +x wp-cli.phar &&
-    mkdir wp &&
-    mv wp-cli.phar wp"
 
     docker exec -it $wordpressContainer bash
 
