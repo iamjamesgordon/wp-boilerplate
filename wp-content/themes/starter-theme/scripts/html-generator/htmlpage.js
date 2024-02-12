4516f42d692935c6a0c3e11ae9c9b0ac917ef2c7
iamjamesgordon <james@neontribe.co.uk>
2024-02-12 18:08:39 +0000

    HTML Generator created with node to convert sass components to static html files
    The HTML Generator has been created with Node using native functions to reduce errors moving forward with new releases.
    The generator has been created as a new script in the scripts folder.
    A HTML folder has been created within  resources/assets.
    A test component has been created in the resources/assets/sass/components folder
    Only components with the html snippet commented with '/* HTML-BUILDER' wrap will be generated into a html file.

---
+++ wp-content/themes/starter-theme/scripts/html-generator/htmlpage.js
@@ -0,0 +1,14 @@
+module.exports.code = (htmlContent) => {
+    const html = `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>HTML 5 Boilerplate</title>
+    </head>
+    <body> \t` + htmlContent + `</body>
+</html>`;
+
+    return html;
+};
