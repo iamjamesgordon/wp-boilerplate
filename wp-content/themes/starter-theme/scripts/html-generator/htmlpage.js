7957d54304b253638355fbd03191b9619c86faa7
iamjamesgordon <james@neontribe.co.uk>
2024-02-12 18:28:18 +0000

    Add function to find the correct asset file path from the manifest.json file

--- wp-content/themes/starter-theme/scripts/html-generator/htmlpage.js
+++ wp-content/themes/starter-theme/scripts/html-generator/htmlpage.js
@@ -1,4 +1,7 @@
+const manifest = require('./manifest');
+
 module.exports.code = (htmlContent) => {
+
     const html = `<!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +9,7 @@ module.exports.code = (htmlContent) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>HTML 5 Boilerplate</title>
+        <link rel="stylesheet" href="` + manifest.asset('main.css') +`">
     </head>
     <body> \t` + htmlContent + `</body>
 </html>`;
